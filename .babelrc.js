efe306a1c6d18f8fc9125286f8d8b7ccc60d46dc
黄凯伦 <340443366@qq.com>
2019-04-01 20:55:12 +0800

    构建配置

--- .babelrc.js
+++ .babelrc.js
@@ -1,10 +1,11 @@
 module.exports = {
   presets: [
-    ['@babel/preset-env', { shippedProposals: true, useBuiltIns: 'usage' }],
+    ['@babel/preset-env', { shippedProposals: true }],
     '@babel/preset-react',
     '@babel/preset-flow',
   ],
   plugins: [
+    '@babel/plugin-transform-react-jsx',
     ['@babel/plugin-proposal-object-rest-spread', { loose: true, useBuiltIns: true }],
     '@babel/plugin-proposal-export-default-from',
     '@babel/plugin-syntax-dynamic-import',
